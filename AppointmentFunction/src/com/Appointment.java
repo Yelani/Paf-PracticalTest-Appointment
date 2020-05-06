5344fc80f4fed48f797257204afd96dad3b8820c
Yelani <yelanisurige1999@gmail.com>
2020-05-06 14:13:25 +0530

    Created the connection with DB

--- AppointmentFunction/src/com/Appointment.java
+++ AppointmentFunction/src/com/Appointment.java
@@ -1,5 +1,28 @@
 package com;
 
+import java.sql.*;
+
 public class Appointment {
+	
+	//CONNECTION
+		private Connection connect()
+		{
+			Connection con = null;
+			
+			try
+			{
+				Class.forName("com.mysql.jdbc.Driver");
+				
+				con = DriverManager.getConnection("Jdbc:mysql://localhost:3306/app", "root", "");  
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace(); 
+			}
+			
+			return con; 
+		}
+	
+	
 
 }
